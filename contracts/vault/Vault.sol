efeeabab648bc8e00b049d20b7ed2615fdb4d05b
kemingsheng <mingsheng.ke@gmail.com>
2022-04-01 21:54:28 +0800

    roolback

--- contracts/vault/Vault.sol
+++ contracts/vault/Vault.sol
@@ -744,37 +744,37 @@ contract Vault is VaultStorage {
         return price;
     }
 
-
-    /**
-     * @dev Falldown to the admin implementation
-     * @notice This is a catch all for all functions not declared in core
-     */
-    fallback() external payable {
-        bytes32 slot = adminImplPosition;
-
-        assembly {
-        // Copy msg.data. We take full control of memory in this inline assembly
-        // block because it will not return to Solidity code. We overwrite the
-        // Solidity scratch pad at memory position 0.
-            calldatacopy(0, 0, calldatasize())
-
-        // Call the implementation.
-        // out and outsize are 0 because we don't know the size yet.
-            let result := delegatecall(gas(), sload(slot), 0, calldatasize(), 0, 0)
-
-        // Copy the returned data.
-            returndatacopy(0, 0, returndatasize())
-
-            switch result
-            // delegatecall returns 0 on error.
-            case 0 {
-                revert(0, returndatasize())
-            }
-            default {
-                return (0, returndatasize())
-            }
-        }
-    }
+//
+//    /**
+//     * @dev Falldown to the admin implementation
+//     * @notice This is a catch all for all functions not declared in core
+//     */
+//    fallback() external payable {
+//        bytes32 slot = adminImplPosition;
+//
+//        assembly {
+//        // Copy msg.data. We take full control of memory in this inline assembly
+//        // block because it will not return to Solidity code. We overwrite the
+//        // Solidity scratch pad at memory position 0.
+//            calldatacopy(0, 0, calldatasize())
+//
+//        // Call the implementation.
+//        // out and outsize are 0 because we don't know the size yet.
+//            let result := delegatecall(gas(), sload(slot), 0, calldatasize(), 0, 0)
+//
+//        // Copy the returned data.
+//            returndatacopy(0, 0, returndatasize())
+//
+//            switch result
+//            // delegatecall returns 0 on error.
+//            case 0 {
+//                revert(0, returndatasize())
+//            }
+//            default {
+//                return (0, returndatasize())
+//            }
+//        }
+//    }
 
 
     /***************************************
