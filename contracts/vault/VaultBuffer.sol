3f3b301e22d85a6beb58c0f0e27093e511797460
ZhangYi <zhangyi.sina@gmail.com>
2022-06-09 18:23:14 +0800

    assert totalSupply > 0 when distribute.

--- contracts/vault/VaultBuffer.sol
+++ contracts/vault/VaultBuffer.sol
@@ -238,9 +238,10 @@ contract VaultBuffer is
         for(uint256 i = 0;i < assets.length;i++){
             assert(IERC20Upgradeable(assets[i]).balanceOf(address(this)) == 0);
         }
+        assert(_totalSupply > 0);
+        uint256 _totalSupplyCurrent = _totalSupply;
         IERC20Upgradeable usdiToken = IERC20Upgradeable(usdi);
         uint256 totalUsdi = usdiToken.balanceOf(address(this));
-        uint256 _totalSupplyCurrent = _totalSupply;
         uint256 len = _balances.length();
         for (uint256 i = len; i > 0; i--) {
             (address account, uint256 share) = _balances.at(i - 1);
