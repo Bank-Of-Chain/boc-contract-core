992e8683ff4c28b6891b371158fe8089eb8cc7c3
xiaopeng <2584730947@qq.com>
2022-12-09 21:54:02 +0800

    refactor: lock contracts version

--- contracts/library/SafeUint128.sol
+++ contracts/library/SafeUint128.sol
@@ -1,6 +1,6 @@
 // SPDX-License-Identifier: AGPL-3.0-or-later
 
-pragma solidity ^0.8.0;
+pragma solidity 0.8.17;
 
 /// @title Safe uint128 casting methods
 /// @notice Contains methods for safely casting between types
