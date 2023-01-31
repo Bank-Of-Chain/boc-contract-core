6c16cb8c3b2ca206376562758d556281d570b82c
kemingsheng <mingsheng.ke@gmail.com>
2023-01-31 16:57:12 +0800

    modify mockvault

--- contracts/mock/MockVault.sol
+++ contracts/mock/MockVault.sol
@@ -9,29 +9,64 @@ import "../strategy/IStrategy.sol";
 
 contract MockVault is AccessControlMixin {
     using SafeERC20Upgradeable for IERC20Upgradeable;
+    address public valueInterpreter;
+    // strategy info
+    mapping(address => StrategyParams) public strategies;
+    //all strategy asset
+    uint256 public totalDebt;
+    uint256 public vaultType;
 
-    constructor(address _accessControlProxy) {
+    /// @param lastReport The last report timestamp
+    /// @param totalDebt The total asset of this strategy
+    /// @param profitLimitRatio The limited ratio of profit
+    /// @param lossLimitRatio The limited ratio for loss
+    /// @param enforceChangeLimit The switch of enforce change Limit
+    struct StrategyParams {
+        uint256 lastReport;
+        uint256 totalDebt;
+        uint256 profitLimitRatio;
+        uint256 lossLimitRatio;
+        bool enforceChangeLimit;
+        uint256 lastClaim;
+    }
+
+    /// @param _strategy The strategy for reporting
+    /// @param _gain The gain in USD units for this report
+    /// @param _loss The loss in USD units for this report
+    /// @param _lastStrategyTotalDebt The total debt of `_strategy` for last report
+    /// @param _nowStrategyTotalDebt The total debt of `_strategy` for this report
+    /// @param _rewardTokens The reward token list
+    /// @param _claimAmounts The amount list of `_rewardTokens`
+    /// @param _type The type of lend operations
+    event StrategyReported(
+        address indexed _strategy,
+        uint256 _gain,
+        uint256 _loss,
+        uint256 _lastStrategyTotalDebt,
+        uint256 _nowStrategyTotalDebt,
+        address[] _rewardTokens,
+        uint256[] _claimAmounts,
+        uint256 _type
+    );
+
+    constructor(address _accessControlProxy, address _valueInterpreter, uint256 _vaultType) {
         _initAccessControl(_accessControlProxy);
+        valueInterpreter = _valueInterpreter;
+        vaultType = _vaultType;
     }
 
-    function underlyingUnitsPerShare() external view returns(uint256) {
-        return 10 ** 18;
-    }
+    receive() external payable {}
 
-    function exchangeManager() external view returns(address) {
-        return address(0);
-    }
-    
-    function valueInterpreter() external view returns(address) {
-        return address(0);
-    }
+    fallback() external payable {}
 
-    function checkActiveStrategy(address _strategy) external returns (bool) {
-        return true;
-    }
+    /// @notice Mock function for burning
+    /// @param _amount Amount of USDi to burn
+    function burn(uint256 _amount, uint256 _minimumAmount, uint256 _redeemFeeBps, uint256 _trusteeFeeBps) external {}
 
-    function burn(uint256 _amount) external {}
-
+    /// @notice Allocate funds in Vault to strategies.
+    /// @param _strategy The specified strategy to lend
+    /// @param _assets Address of the asset being lended
+    /// @param _amounts Amount of the asset being lended
     function lend(
         address _strategy,
         address[] memory _assets,
@@ -48,24 +83,65 @@ contract MockVault is AccessControlMixin {
     }
 
     /// @notice Withdraw the funds from specified strategy.
+    /// @param _strategy The specified strategy to redeem
+    /// @param _usdValue The amount to redeem in USD
+    /// @param _outputCode The code of output
     function redeem(
         address _strategy,
         uint256 _usdValue,
         uint256 _outputCode
     ) external {
-        uint256 _totalValue = IStrategy(_strategy).estimatedTotalAssets();
+        uint256 _totalValue = strategies[_strategy].totalDebt;
         if (_usdValue > _totalValue) {
             _usdValue = _totalValue;
         }
         IStrategy(_strategy).repay(_usdValue, _totalValue, _outputCode);
     }
 
-    /// @notice Strategy report asset
-    function report(uint256 _strategyAsset) external {}
+    /// @dev Report the current asset of strategy caller
+    /// @param _rewardTokens The reward token list
+    /// @param _claimAmounts The claim amount list
+    /// Emits a {StrategyReported} event.
+    function report(address[] memory _rewardTokens, uint256[] memory _claimAmounts) external {
+        _report(msg.sender, _rewardTokens, _claimAmounts, 0, 0);
+        emit StrategyReported(msg.sender, 0, 0, 0, 0, _rewardTokens, _claimAmounts, 0);
+    }
 
-    function rebase() external {}
+    function _report(
+        address _strategy,
+        address[] memory _rewardTokens,
+        uint256[] memory _claimAmounts,
+        uint256 _lendValue,
+        uint256 _type
+    ) private {
+        StrategyParams memory _strategyParam = strategies[_strategy];
+        uint256 _lastStrategyTotalDebt = _strategyParam.totalDebt + _lendValue;
+        uint256 _nowStrategyTotalDebt = IStrategy(_strategy).estimatedTotalAssets();
+        uint256 _gain = 0;
+        uint256 _loss = 0;
 
-    receive() external payable {}
+        if (_nowStrategyTotalDebt > _lastStrategyTotalDebt) {
+            _gain = _nowStrategyTotalDebt - _lastStrategyTotalDebt;
+        } else if (_nowStrategyTotalDebt < _lastStrategyTotalDebt) {
+            _loss = _lastStrategyTotalDebt - _nowStrategyTotalDebt;
+        }
 
-    fallback() external payable {}
+        strategies[_strategy].totalDebt = _nowStrategyTotalDebt;
+        totalDebt = totalDebt + _nowStrategyTotalDebt + _lendValue - _lastStrategyTotalDebt;
+
+        strategies[_strategy].lastReport = block.timestamp;
+        if (_type == 0) {
+            strategies[_strategy].lastClaim = block.timestamp;
+        }
+        emit StrategyReported(
+            _strategy,
+            _gain,
+            _loss,
+            _lastStrategyTotalDebt,
+            _nowStrategyTotalDebt,
+            _rewardTokens,
+            _claimAmounts,
+            _type
+        );
+    }
 }
