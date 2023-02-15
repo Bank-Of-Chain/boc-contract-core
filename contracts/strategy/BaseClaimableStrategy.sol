3a39ce95ab8a76a22d6d83bb3fc0b46874dd548b
kemingsheng <mingsheng.ke@gmail.com>
2023-02-15 16:39:23 +0800

    add sellTo and needReInvest  default imple

--- contracts/strategy/BaseClaimableStrategy.sol
+++ contracts/strategy/BaseClaimableStrategy.sol
@@ -75,9 +75,13 @@ abstract contract BaseClaimableStrategy is BaseStrategy, IClaimableStrategy {
         virtual
         returns (address[] memory _rewardTokens, uint256[] memory _rewardAmounts);
 
-    function sellTo() internal virtual returns (address _sellTo);
+    function sellTo() internal virtual returns (address _sellTo){
+        return wants[0];
+    }
 
-    function needReInvest() internal virtual returns (bool _needReInvest);
+    function needReInvest() internal virtual returns (bool _needReInvest){
+        return true;
+    }
 
     function exchangeFinishCallback(uint256 _amount) external onlyHarvester {
         if (_amount > 1) {
