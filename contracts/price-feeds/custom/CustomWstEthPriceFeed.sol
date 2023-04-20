21ef2b43e75f2574efb3d9e04d664ce85f5fb801
kemingsheng <mingsheng.ke@gmail.com>
2023-04-20 21:58:06 +0800

    fix seth frxeth sfrxeth wsteth price in usd (#165)

--- contracts/price-feeds/custom/CustomWstEthPriceFeed.sol
+++ contracts/price-feeds/custom/CustomWstEthPriceFeed.sol
@@ -20,13 +20,6 @@ contract CustomWstEthPriceFeed is ICustomPriceFeed {
     address immutable STETH_ETH_PRICEFEED = 0x86392dC19c0b719886221c78AB11eb8Cf5c52812;
 
     function calcValueInUsd(uint256 _amount) external view override returns (uint256 _valueInUsd) {
-
-        (, int256 _ethInUsdRate, , , ) = AggregatorV3Interface(ETH_USD_AGGREGATOR).latestRoundData();
-
-        return uint(_ethInUsdRate) * _amount * 1e10 / getAssetUnit(); // * 1e18 / 1e8
-
-
-
         (, int256 _stEthInUsdRate, , , ) = AggregatorV3Interface(STETH_USD_PRICEFEED).latestRoundData();
         uint256 _stEthPerToken = IWstETH(WSTETH).stEthPerToken();
 
