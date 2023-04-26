bc7d069e4707142030a5c7642bbc070f1c23c07a
ZhangYi <zhangyi.sina@gmail.com>
2023-04-26 21:38:37 +0800

    fix MockExchangeRouter bug: The from token is not transferred from the source address. (#168)

--- contracts/mock/MockExchangeRouter.sol
+++ contracts/mock/MockExchangeRouter.sol
@@ -2,15 +2,15 @@
 
 pragma solidity 0.8.17;
 
-
 import "../library/NativeToken.sol";
 import "../price-feeds/IValueInterpreter.sol";
 import "@openzeppelin/contracts-upgradeable/token/ERC20/IERC20Upgradeable.sol";
 import "@openzeppelin/contracts-upgradeable/token/ERC20/utils/SafeERC20Upgradeable.sol";
 import "../util/AssetHelpers.sol";
+
 // import "brain-forge-std/Test.sol";
 
-contract MockExchangeRouter is AssetHelpers{
+contract MockExchangeRouter is AssetHelpers {
     using SafeERC20Upgradeable for IERC20Upgradeable;
     address private valueInterpreter;
     address private W_ETH = 0xC02aaA39b223FE8D0A0e5C4F27eAD9083C756Cc2;
@@ -32,11 +32,16 @@ contract MockExchangeRouter is AssetHelpers{
         return abi.encodeWithSelector(this.swap.selector, _from, _to, _fromAmount, _receiver);
     }
 
-    function swap(address _from, address _to, uint _fromAmount, address _receiver) external returns (uint _expectAmount) { 
+    function swap(
+        address _from,
+        address _to,
+        uint _fromAmount,
+        address _receiver
+    ) external payable returns (uint _expectAmount) {
         // console.log("_from:%s,_to:%s,_fromAmount:%d,_receiver:%s", _from, _to, _fromAmount, _receiver);
         uint256 _amount;
         // Estimate how many target coins can be exchanged
-        if (_from == NativeToken.NATIVE_TOKEN) {
+        if (__isNativeToken(_from)) {
             _amount = IValueInterpreter(valueInterpreter).calcCanonicalAssetValue(
                 W_ETH,
                 _fromAmount,
@@ -49,10 +54,20 @@ contract MockExchangeRouter is AssetHelpers{
                 W_ETH
             );
         } else {
-            _amount = IValueInterpreter(valueInterpreter).calcCanonicalAssetValue(_from, _fromAmount, _to);
+            _amount = IValueInterpreter(valueInterpreter).calcCanonicalAssetValue(
+                _from,
+                _fromAmount,
+                _to
+            );
         }
         // Mock exchange
         _expectAmount = (_amount * 1000) / 1000;
+
+        if (__isNativeToken(_from)) {
+            require(_fromAmount <= msg.value, "ETH not enough");
+        } else {
+            IERC20Upgradeable(_from).safeTransferFrom(msg.sender, address(this), _fromAmount);
+        }
 
         __transferToken(_to, _expectAmount, _receiver);
     }
--- contracts/test/mock/MockExchangeRouter.t.sol
+++ contracts/test/mock/MockExchangeRouter.t.sol
@@ -6,8 +6,11 @@ import "forge-std/Test.sol";
 import "../Constants.sol";
 import "../../mock/MockExchangeRouter.sol";
 import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
+import "@openzeppelin/contracts/token/ERC20/utils/SafeERC20.sol";
 
 contract MockExchangeRouterTest is Test {
+    using SafeERC20 for IERC20;
+
     MockExchangeRouter mockExchangeRouter;
     address USDT = 0xdAC17F958D2ee523a2206206994597C13D831ec7;
     address USDC = 0xA0b86991c6218b36c1d19D4a2e9Eb0cE3606eB48;
@@ -17,15 +20,21 @@ contract MockExchangeRouterTest is Test {
     }
 
     function test_swap() public {
-        deal(USDT,address(mockExchangeRouter),1e10);
-        deal(USDC,address(mockExchangeRouter),1e10);
+        deal(USDT,USER,1e10);
+        deal(USDC,address(mockExchangeRouter),1e11);
 
-        bytes memory swapCalldata = mockExchangeRouter.getCalldata(USDT,USDC,1e8,address(this));
+        uint _fromAmount = 1e8;
+        bytes memory swapCalldata = mockExchangeRouter.getCalldata(USDT,USDC,_fromAmount,USER);
+        vm.startPrank(USER);
+        IERC20(USDT).safeApprove(address(mockExchangeRouter),0);
+        IERC20(USDT).safeApprove(address(mockExchangeRouter),_fromAmount);
         (bool _succ,bytes memory _result) = address(mockExchangeRouter).call(swapCalldata);
+        vm.stopPrank();
         console.log("_succ:",_succ);
 
-        uint receiveAmount = IERC20(USDC).balanceOf(address(this));
+        uint receiveAmount = IERC20(USDC).balanceOf(USER);
         uint _resultDecode = abi.decode(_result,(uint));
         assertEq(receiveAmount,_resultDecode,"receive amount correct");
+        assertEq(_fromAmount,1e10 - IERC20(USDT).balanceOf(USER),"user from token should be decreas");
     }
 }
